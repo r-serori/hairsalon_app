f56a99513931bc272362e6b9e6a348e64c75b55b
r-serori <ryunosuke.sakaue@gmail.com>
2024-07-24 19:01:53 +0900

    cors.php

--- config/cors.php
+++ config/cors.php
@@ -2,94 +2,36 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
     'paths' => [
         'api/*',
         'sanctum/csrf-cookie',
-        'csrf-token',
-        'getKey',
-        'storage',
-        'storage/*',
+        'login',
+        'register',
         'forgotPassword',
         'resetPassword',
         'verify-email/*',
         'updateInfo/*',
-        'updateUser',
-        'updateUserPassword',
-        'login',
-        'login/*',
-        'register',
-        'register/*',
-        'dashboard',
-        'ownerRegister',
-        'ownerRegister/*',
-        'dashboard/*',
         'logout',
-        'logout/*',
-        'user',
-        'user/*',
-        'attendances',
-        'attendances/*',
-        'attendance_times',
+        'getKey',
+        'csrf-token',
         'attendance_times/*',
-        'courses',
-        'courses/*',
-        'merchandises',
-        'merchandises/*',
-        'customers',
-        'customers/*',
-        'customers/*/schedule',
-        'daily_sales',
-        'daily_sales/*',
-        'daily_sales/update-daily-sales',
-        'hairstyles',
-        'hairstyles/*',
-        'monthly_sales',
-        'monthly_sales/*',
-        'monthly_sales/update-monthly-sales',
-        'options',
-        'options/*',
-        'schedules',
-        'schedules/*',
-        'schedules/update-daily-sales',
-        'yearly_sales',
-        'yearly_sales/*',
-        'stocks',
-        'stocks/*',
-        'stock_categories',
-        'stock_categories/*',
-        'customer_schedules',
-        'course_customers',
-        'customer_attendances',
-        'hairstyle_customers',
-        'hairstyle_schedules',
-        'merchandise_customers',
-        'merchandise_schedules',
-        'option_customers',
-        'option_schedules',
-        'attendance_attendancesTimes',
-        'attendance_attendancesTimes/*',
-        'images',
-        'images/*',
-        'firstAttendanceTimes',
         'firstAttendanceTimes/*',
+        'customers/*',
+        'courses/*',
+        'options/*',
+        'merchandises/*',
+        'hairstyles/*',
+        'schedules/*',
+        'daily_sales/*',
+        'monthly_sales/*',
+        'yearly_sales/*',
+        'stocks/*',
+        'stock_categories/*',
     ],
 
     'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => [env('FRONTEND_URL')],
 
     'allowed_origins_patterns' => [],
 
